2a77bd6768ae2d6572b8d7648a9b8b9b174db758
Edward Rajah <edrayel@outlook.com>
2019-11-09 22:12:15 +0100

    Update Messages.php

--- application/controllers/Messages.php
+++ application/controllers/Messages.php
@@ -3,14 +3,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Messages extends CI_Controller {
 
-    public function __construct()
-    {
-        header("Access-Control-Allow-Origin: *");
-        parent::__construct();
-    }
+    // public function __construct()
+    // {
+    //     parent::__construct();
+    // }
 
 	public function get(string $id, string $sender = null, string $receiver = null)
 	{
+        header("Access-Control-Allow-Origin: GET");
+
         if ($sender == null) {
             $this->output
             ->set_content_type('application/json')
@@ -28,6 +29,8 @@ class Messages extends CI_Controller {
     
     public function get_all(string $username, string $receiver = null)
     {
+        header("Access-Control-Allow-Origin: GET");
+
         if ($receiver == null) {
             $this->output
             ->set_content_type('application/json')
@@ -41,6 +44,8 @@ class Messages extends CI_Controller {
 
     public function create()
     {
+        header("Access-Control-Allow-Origin: POST");
+
         $record = array(
             "date" => date(DATE_RFC822),
             "time" => unix_to_human(time(), TRUE),
@@ -67,6 +72,8 @@ class Messages extends CI_Controller {
 
     public function update(int $id)
     {
+        header("Access-Control-Allow-Origin: PUT");
+
         $data = array(
             "date" => $this->input->post("date"),
             "time" => $this->input->post("time"),
@@ -80,6 +87,8 @@ class Messages extends CI_Controller {
 
     public function delete(int $id)
     {
+        header("Access-Control-Allow-Origin: DELETE");
+
         $this->messages_model->delete($id);
     }
 }
