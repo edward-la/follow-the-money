894efe0c5c43a34dff3c37e19b76094a5e8283bc
Edward Rajah <edrayel@outlook.com>
2019-11-09 22:28:56 +0100

    Update Messages.php

--- application/controllers/Messages.php
+++ application/controllers/Messages.php
@@ -6,6 +6,7 @@ class Messages extends CI_Controller {
     public function __construct()
     {
         header("Access-Control-Allow-Origin: *");
+        header("Access-Control-Allow-Headers: *");
         header("Access-Control-Allow-Methods: *");
         parent::__construct();
     }
