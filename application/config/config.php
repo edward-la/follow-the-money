566b78521a2883fcb9a3d4c1129f8a4c9a86a1a7
Edward Rajah <edrayel@outlook.com>
2019-11-09 16:03:15 +0100

    Update database config

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8888/ftm/';
+$config['base_url'] = 'https://follow-the-money-2019.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
