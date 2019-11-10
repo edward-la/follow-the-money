6fb3129638c5f7cfb8fdc8c2920a7afa257e00fe
Edward Rajah <edrayel@outlook.com>
2019-11-10 08:44:40 +0100

    Messages.php & .htaccess

--- application/controllers/Messages.php
+++ application/controllers/Messages.php
@@ -5,9 +5,9 @@ class Messages extends CI_Controller {
 
     public function __construct()
     {
-        header("Access-Control-Allow-Origin: *");
+        header("Access-Control-Allow-Origin: ftm-web.herokuapp.com");
         header("Access-Control-Allow-Headers: *");
-        header("Access-Control-Allow-Methods: *");
+        header("Access-Control-Allow-Methods: GET, POST");
         parent::__construct();
     }
 
